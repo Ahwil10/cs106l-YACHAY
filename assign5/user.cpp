885f2b4e8d6ac3205f8971dc9ad327e575effa2f
Ahwill10 <lorsahwill@gmail.com>
2025-04-04 15:04:50 -0500

    updated

--- assign5/user.cpp
+++ assign5/user.cpp
@@ -1,8 +1,9 @@
 #include "user.h"
+#include <sstream>
+#include <algorithm>  // for std::copy
+#include <stdexcept>  // for std::out_of_range
 
-/**
- * Creates a new User with the given name and no friends.
- */
+// Constructor
 User::User(const std::string& name)
   : _name(name)
   , _friends(nullptr)
@@ -11,55 +12,100 @@ User::User(const std::string& name)
 {
 }
 
-/**
- * Adds a friend to this User's list of friends.
- * @param name The name of the friend to add.
- */
-void
-User::add_friend(const std::string& name)
+// Destructor
+User::~User() {
+  delete[] _friends;
+}
+
+// Copy constructor
+User::User(const User& other)
+  : _name(other._name)
+  , _size(other._size)
+  , _capacity(other._capacity)
+  , _friends(other._size ? new std::string[other._capacity] : nullptr)
 {
-  if (_size == _capacity) {
-    _capacity = 2 * _capacity + 1;
-    std::string* newFriends = new std::string[_capacity];
-    for (size_t i = 0; i < _size; ++i) {
-      newFriends[i] = _friends[i];
+    if (_friends) {
+        std::copy(other._friends, other._friends + _size, _friends);
     }
-    delete[] _friends;
-    _friends = newFriends;
-  }
-
-  _friends[_size++] = name;
 }
 
-/**
- * Returns the name of this User.
- */
-std::string
-User::get_name() const
-{
-  return _name;
+// Copy assignment operator
+User& User::operator=(const User& other) {
+    if (this != &other) {  // Check for self-assignment
+        // Create new array first (exception-safe)
+        std::string* newFriends = nullptr;
+        if (other._size > 0) {
+            newFriends = new std::string[other._capacity];
+            std::copy(other._friends, other._friends + other._size, newFriends);
+        }
+
+        // Delete old array
+        delete[] _friends;
+
+        // Assign new values
+        _name = other._name;
+        _size = other._size;
+        _capacity = other._capacity;
+        _friends = newFriends;
+    }
+    return *this;
 }
 
-/**
- * Returns the number of friends this User has.
- */
-size_t
-User::size() const
-{
-  return _size;
+// Add friend
+void User::add_friend(const std::string& name) {
+    if (_size == _capacity) {
+        _capacity = (_capacity == 0) ? 1 : _capacity * 2;
+        std::string* newFriends = new std::string[_capacity];
+        for (size_t i = 0; i < _size; ++i) {
+            newFriends[i] = _friends[i];
+        }
+        delete[] _friends;
+        _friends = newFriends;
+    }
+    _friends[_size++] = name;
 }
 
-/**
- * Sets the friend at the given index to the given name.
- * @param index The index of the friend to set.
- * @param name The name to set the friend to.
- */
-void User::set_friend(size_t index, const std::string& name)
-{
-  _friends[index] = name;
+// Get name
+std::string User::get_name() const {
+    return _name;
 }
 
-/** 
- * STUDENT TODO:
- * The definitions for your custom operators and special member functions will go here!
- */
+// Get size
+size_t User::size() const {
+    return _size;
+}
+
+// Set friend
+void User::set_friend(size_t index, const std::string& name) {
+    if (index >= _size) {
+        throw std::out_of_range("Friend index out of range");
+    }
+    _friends[index] = name;
+}
+// += operator for mutual friendship
+User& User::operator+=(User& rhs) {
+    // Add rhs's name to this user's friend list
+    this->add_friend(rhs.get_name());
+    // Add this user's name to rhs's friend list
+    rhs.add_friend(this->get_name());
+    return *this;  // Return reference to self for chaining
+}
+
+// < operator for name comparison
+bool User::operator<(const User& rhs) const {
+    // Compare names lexicographically
+    return this->_name < rhs._name;
+}
+
+// Output operator
+std::ostream& operator<<(std::ostream& os, const User& user) {
+    os << "User(name=" << user._name << ", friends=[";
+    for(size_t i = 0; i < user._size; ++i) {
+        os << user._friends[i];
+        if (i != user._size - 1) {
+            os << ", ";
+        }
+    }
+    os << "])";
+    return os;
+}
--- assign5/user.h
+++ assign5/user.h
@@ -2,27 +2,42 @@
  * CS106L Assignment 5: TreeBook
  * Created by Fabio Ibanez with modifications by Jacob Roberts-Baca.
  */
-
+#ifndef USER_H
+#define USER_H
 #include <iostream>
 #include <string>
 
 class User
 {
 public:
-  User(const std::string& name);
-  void add_friend(const std::string& name);
-  std::string get_name() const;
-  size_t size() const;
-  void set_friend(size_t index, const std::string& name);
-
-  /** 
-   * STUDENT TODO:
-   * Your custom operators and special member functions will go here!
-   */
-
+   // Constructor
+   User(const std::string& name);
+    
+   // Destructor
+   ~User();
+   
+   // Copy operations
+   User(const User& user);
+   User& operator=(const User& user);
+   
+   // Move operations (deleted)
+   User(User&& user) = delete;
+   User& operator=(User&& user) = delete;
+   
+   // Member functions
+   void add_friend(const std::string& name);
+   std::string get_name() const;
+   size_t size() const;
+   void set_friend(size_t index, const std::string& name);
+   // Overload
+   User& operator+=(User& rhs);  // Add mutual friendship
+   bool operator<(const User& rhs) const;  // Compare by name
+   // Friend function for output
+   friend std::ostream& operator<<(std::ostream& os, const User& user);
 private:
   std::string _name;
   std::string* _friends;
   size_t _size;
   size_t _capacity;
 };
+#endif // USER_H
